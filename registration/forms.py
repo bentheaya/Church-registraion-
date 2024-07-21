0368e9cc7128b2b705743a85cef3df58f990c62e
Benaiah Galavu <bentheaya@gmail.com>
2024-07-21 05:08:07 +0300

    Final

--- registration/forms.py
+++ registration/forms.py
@@ -4,9 +4,17 @@ from .models import Member
 class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
-        fields = '__all__'
+        fields = [
+            'full_name', 'date_of_birth', 'gender', 'phone_number', 'email_address', 
+            'mailing_address', 'marital_status', 'spouse_name', 'children_names_ages',
+            'membership_status', 'date_of_membership', 'baptism_info', 'previous_church', 
+            'ministry_interests', 'spiritual_gifts_talents', 'emergency_contact_name', 
+            'emergency_contact_relationship', 'emergency_contact_phone', 'special_needs_allergies',
+            'preferred_service_time', 'comments_notes', 'photo_consent', 'privacy_consent'
+        ]
+
         widgets = {
             'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
             'date_of_membership': forms.DateInput(attrs={'type': 'date'}),
-            'baptism_date': forms.DateInput(attrs={'type': 'date'}),
+            'preferred_service_time': forms.TimeInput(attrs={'type': 'time'}),
         }
--- registration/models.py
+++ registration/models.py
@@ -6,14 +6,12 @@ class Member(models.Model):
         ('F', 'Female'),
         ('O', 'Other'),
     ]
-
     MARITAL_STATUS_CHOICES = [
         ('S', 'Single'),
         ('M', 'Married'),
         ('D', 'Divorced'),
         ('W', 'Widowed'),
     ]
-
     MEMBERSHIP_STATUS_CHOICES = [
         ('N', 'New member'),
         ('E', 'Existing member'),
@@ -25,22 +23,21 @@ class Member(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     phone_number = models.CharField(max_length=15)
     email_address = models.EmailField()
-    mailing_address = models.CharField(max_length=200)
+    mailing_address = models.CharField(max_length=255)
     marital_status = models.CharField(max_length=1, choices=MARITAL_STATUS_CHOICES)
     spouse_name = models.CharField(max_length=100, blank=True, null=True)
     children_names_ages = models.TextField(blank=True, null=True)
     membership_status = models.CharField(max_length=1, choices=MEMBERSHIP_STATUS_CHOICES)
     date_of_membership = models.DateField()
-    baptism_date = models.DateField(blank=True, null=True)
-    baptism_place = models.CharField(max_length=100, blank=True, null=True)
-    previous_church = models.CharField(max_length=100, blank=True, null=True)
-    ministry_interests = models.TextField(blank=True, null=True)
-    spiritual_gifts_talents = models.TextField(blank=True, null=True)
+    baptism_info = models.CharField(max_length=255, blank=True, null=True)
+    previous_church = models.CharField(max_length=255, blank=True, null=True)
+    ministry_interests = models.CharField(max_length=255, blank=True, null=True)
+    spiritual_gifts_talents = models.CharField(max_length=255, blank=True, null=True)
     emergency_contact_name = models.CharField(max_length=100)
     emergency_contact_relationship = models.CharField(max_length=50)
-    emergency_contact_phone_number = models.CharField(max_length=15)
+    emergency_contact_phone = models.CharField(max_length=15)
     special_needs_allergies = models.TextField(blank=True, null=True)
-    preferred_service_time = models.CharField(max_length=50, blank=True, null=True)
+    preferred_service_time = models.TimeField(blank=True, null=True)
     comments_notes = models.TextField(blank=True, null=True)
     photo_consent = models.BooleanField(default=False)
     privacy_consent = models.BooleanField(default=False)
